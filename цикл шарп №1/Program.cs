6dd4aacb633cdb04743ee926bfb8fe58ad8a2a9f
Eagle Talibal <eagle_talibal@mail.ru>
2019-10-31 09:23:27 +0300

    first commit

---
+++ цикл шарп №1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace цикл_шарп__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сумма чисел от -12 до -65 ");
+            int a = -12;
+            int sum = 0;
+            int b = -65;
+            int c = 0;
+            while (a>-65)
+            {
+                sum = sum + a;
+                a--;
+                b++;
+               
+            }
+            Console.WriteLine(sum);
+
+
+            
+            Console.WriteLine("Среднее арифметическое от -12 до -65");
+            Console.WriteLine(c-=sum/b);
+
+            Console.ReadKey();
+        }
+    }
+}
